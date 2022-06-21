a4794a9998f06786b75191fbebd1f1c5b5296987
Eric <ericfaiscaextremo@gmail.com>
2022-06-21 15:39:43 -0300

    acessar e login

--- cypress/e2e/1-getting-started/acessar_link.cy.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-acessar_link('acessar_link', () => {
-  it('acessar_link', function() {
-    cy.visit('http://staging.clippfacil.com.br:8000/#/login/');
-  });
-});
---
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+context('acessar_link', () => {
+
+  it('acessar_link', function() {
+    cy.visit('http://staging.clippfacil.com.br:8000/#/login/');
+  });
+
+  it('login', function() {
+    cy.get('form .ng-valid-email').type('andreiars@compufacil.com.br');
+    cy.get('form .ng-invalid-required').type('123456');
+    cy.get('form button').click();
+  });
+
+});
